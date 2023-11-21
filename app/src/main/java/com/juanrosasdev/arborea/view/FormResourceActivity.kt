91e6cceb69dac67aaa18949f7ba4f4aeed90faef
JuanRosas <juanmrosas8@gmail.com>
2023-11-21 19:01:55 -0300

    add new FormResourceActivity and slider

---
+++ app/src/main/java/com/juanrosasdev/arborea/view/FormResourceActivity.kt
@@ -0,0 +1,12 @@
+package com.juanrosasdev.arborea.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanrosasdev.arborea.R
+
+class FormResourceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_resource)
+    }
+}
