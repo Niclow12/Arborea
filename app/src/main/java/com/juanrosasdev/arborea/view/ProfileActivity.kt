8aa682de88e432bbd05754941d146248f5625ed2
JuanRosas <juanmrosas8@gmail.com>
2023-11-21 14:39:19 -0300

    new activity ProfileActivity for users

---
+++ app/src/main/java/com/juanrosasdev/arborea/view/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.juanrosasdev.arborea.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juanrosasdev.arborea.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
